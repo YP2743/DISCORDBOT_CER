6b76fb8c90081b2b44a60d5a61aca68fc047d33b
YP2743 <truongducvt123@gmail.com>
2022-12-05 23:29:54 +0700

    DISCORDBOT_CER

--- sources/server/controllers/RoomController.js
+++ sources/server/controllers/RoomController.js
@@ -1,9 +1,21 @@
 const Room = require("../models/RoomModel");
 const UsableRooms = require("./UsableRoomsList");
+const Date_Array = require("./Date_Array");
+const Month_Array = require("./Month_Array");
+const February_Date = require("./February_Date");
 
 async function FindFreeRoomsByDate(date_data, month_data, year_data) {
   try {
-    if (!date_data) return;
+    if (
+      !date_data ||
+      !Date_Array.includes(date_data) ||
+      !Month_Array.includes(month_data.toString()) ||
+      (year_data.toString() !== "2022" && year_data.toString() !== "2023")
+    )
+      return;
+    else if (month_data.toString() === "2") {
+      if (!February_Date.includes(date_data)) return;
+    }
     let Date_Format = `${year_data}-${
       month_data < 10 ? "0" + month_data : month_data
     }-${date_data < 10 ? "0" + date_data : date_data}`;
@@ -26,7 +38,7 @@ async function FindDataByDate(date_data, month_data, year_data) {
       month_data < 10 ? "0" + month_data : month_data
     }-${date_data < 10 ? "0" + date_data : date_data}`;
     let regex = new RegExp(Date_Format, "i");
-    return data = await Room.find({ Date: regex });
+    return (data = await Room.find({ Date: regex }));
   } catch (error) {
     console.log(error);
   }
@@ -42,12 +54,22 @@ function FindFreeRoomsForSlot(data, slot) {
   return UsableRooms.filter((x) => !arr.includes(x));
 }
 
-function FindFreeRoomsForSlotExcludedAllDay(slot=[],allday=[]){
-  return slot.filter(x => !allday.includes(x));
+function FindFreeRoomsForSlotExcludedAllDay(slot = [], allday = []) {
+  let slot_excludedAD = slot.filter((x) => !allday.includes(x));
+  if (slot_excludedAD.length === 0) {
+    slot_excludedAD.push("Check All Day List !!!");
+  }
+  return slot_excludedAD;
 }
 
 function FindSameString(arr = [], ...arrays) {
-  return arr.filter((a) => arrays.every((b) => b.includes(a)));
+  let arr__ = [
+    "No Consecutive Empty Rooms or All Rooms are already consecutive !!!",
+  ];
+  let arr_ = arr.filter((a) => arrays.every((b) => b.includes(a)));
+  if (arr_.length === 0 || arr_.includes("Check All Day List !!!")) {
+    return arr__;
+  } else return arr_;
 }
 
 module.exports = {
@@ -55,5 +77,5 @@ module.exports = {
   FindSameString,
   FindFreeRoomsByDate,
   FindFreeRoomsForSlotExcludedAllDay,
-  FindDataByDate
+  FindDataByDate,
 };
